5de78d0ea67188851acf4e2e3854f871c3e48fd8
liangchow <liangchern@gmail.com>
2025-07-23 22:29:08 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,25 @@ import './index.css'
 
 function App() {
 
+  const [todos, setTodos] = useState([
+    "I have a dream",
+    "It's in the brew"  
+  ])
+
+
   return (
-    <div>
+    <div className='flex flex-col flex-1 gap-10'>
+      <ul className='flex flex-col flex-1 gap-1 p-4'>
+        {todos.map((todo) => (
+          <li className="max-w-[1000px] w-full mx-auto flex justify-between items-center bg-white mt-2 p-2 hover:shadow-lg rounded-3xl border-2 border-solid border-indigo-300">
+            {todo}
+          </li>
+        ))}
+
+      </ul>
     </div>
   )
+
 }
 
 export default App
