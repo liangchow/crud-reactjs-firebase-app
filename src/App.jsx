489239c4af72142a71ee0a73b8f7d623606f2e1c
liangchow <liangchern@gmail.com>
2025-07-25 02:20:47 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   const [todos, setTodos] = useState([
     {firstName: "Joe", lastName: "Doe", comment: "You are awesome!", rating: 4, status: true, src: ""},
     {firstName: "Simone", lastName: "Ming", comment: "You're the best ;) I have not met people like you. I am wishing you the best in your future endeavors", rating: 5, status: false, src: ""},
+    {firstName: "Jane", lastName: "Moon", comment: "Aiks! You are getting there.", rating: 4, status: true, src: ""}
     ])
 
   function handleToggleStatus(index){
@@ -41,18 +42,35 @@ function App() {
               <div className='flex ml-1 w-15'>
                 {headshot(todo)}
               </div>
-              <div className='flex flex-col ml-1 w-full'>     
+              <div className={'flex flex-col ml-1 w-full ' + (todo.status == true ? ' opacity-50' : '')}>     
                 <span className='text-lg sm:text-xl font-semibold '>{todo.firstName} {todo.lastName}</span>
                 <span className='text-base sm:text-lg'>{todo.comment}</span>
               </div>
               <div className='flex p-2 ml-1 text-nowrap '>⭐ {todo.rating}/5</div>
               <div className='flex flex-col items-center p-2 gap-2 '>
-                <button onClick={() => handleToggleStatus(todoIndex)}><i className={"text-indigo-600 hover:text-indigo-400 cursor-pointer transition " + (todo.status==true ? " fa-solid fa-eye-slash" : " fa-solid fa-eye")}></i></button>
+                <button onClick={() => handleToggleStatus(todoIndex)}><i className={"text-indigo-600 hover:text-indigo-400 cursor-pointer transition " + (todo.status == true ? " fa-solid fa-eye-slash opacity-50" : " fa-solid fa-eye")}></i></button>
               </div>
             </li>
           ))) 
           : (<p>Looks like something is missing</p>)
         }
+      </ul>
+
+      <h2 className='text-indigo-800 text-center'>Display Below:</h2>
+
+      <ul className='flex flex-col flex-1 gap-1 p-4'>
+        {todos?.map((todo, todoIndex) => (
+            <li key={todoIndex} className="max-w-[1000px] w-full mx-auto flex justify-between items-center bg-white mt-2 p-2 hover:shadow-lg rounded-3xl border-2 border-solid border-indigo-300">
+              <div className='flex ml-1 w-15'>
+                {headshot(todo)}
+              </div>
+              <div className='flex flex-col ml-1 w-full'>     
+                <span className='text-lg sm:text-xl font-semibold '>{todo.firstName} {todo.lastName}</span>
+                <span className='text-base sm:text-lg'>{todo.comment}</span>
+              </div>
+              <div className='flex p-2 ml-1 text-nowrap '>⭐ {todo.rating}/5</div>
+            </li>
+          ))}
       </ul>
     </div>
   )
