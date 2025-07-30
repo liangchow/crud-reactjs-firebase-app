2e47e1a282e1f1cd7f52a1c2600afe5438891692
liangchow <liangchern@gmail.com>
2025-07-29 23:22:24 -0700

    Create App_template.jsx

---
+++ src/App_template.jsx
@@ -0,0 +1,116 @@
+import { useState } from 'react'
+import './index.css'
+
+// https://www.javascripttutorial.net/react-tutorial/react-todo-app/
+// https://upmostly.com/tutorials/build-a-todo-app-in-react-using-hooks
+
+function App() {
+
+  const [todos, setTodos] = useState([
+    {firstName: "Joe", lastName: "Doe", comment: "You are awesome!", rating: 4, status: true, src: ""},
+    {firstName: "Simone", lastName: "Ming", comment: "You're the best ;) I have not met people like you. I am wishing you the best in your future endeavors", rating: 5, status: false, src: ""},
+    {firstName: "Jane", lastName: "Moon", comment: "Aiks! You are getting there.", rating: 4, status: true, src: ""}
+    ])
+  const [comment, setComment] = useState('')
+  const [rating, setRating] = useState(0)
+  const [hover, setHover] = useState(null)
+
+  function handleAddTodo(newFeedback, newRating){
+    const newTodo = {firstName: "New", lastName: "Person", comment: newFeedback, rating: newRating, status: true, src: ""}
+    const updatedTodos = [newTodo, ...todos]
+    setTodos(updatedTodos)
+  }
+
+  function handleToggleStatus(index){
+    const updatedTodos = [...todos]
+    updatedTodos[index].status = !updatedTodos[index].status
+    setTodos(updatedTodos)
+    console.log(updatedTodos[index])
+    }
+
+  function headshot(peer){
+    if (peer.src) {
+      return (
+        <image className='rounded-full border border-solid border-indigo-500 ' src={peer.src} width={40} height={40} style={{objectFit: "contain"}} alt="pp" />
+    )} else {
+      return (
+        <div className='size-[40px] rounded-full bg-indigo-500 inline-flex items-center justify-center '>
+            <p className={'text-base text-indigo-50 '}>{peer.firstName[0]}{peer.lastName[0]}</p>
+        </div>
+      )
+    }
+  }
+
+  // Create todo
+  // Read todo from firebase
+  // Update todo in firevase
+  // Delete todo
+
+
+  return (
+    <div className='flex flex-col flex-1 gap-4'>
+      
+      <div className='flex flex-1 w-full justify-center gap-2 p-4'>
+        <form >
+          <input className='rounded-3xl border-2 border-solid border-indigo-300 px-2' placeholder="Enter feedback" value={comment} onChange={(e) => setComment(e.target.value)} />
+          {[...Array(5)].map((star, starIndex) => {
+            starIndex += 1
+            return (
+                  <button type='button' key={starIndex} className={'text-2xl cursor-pointer opacity-50 ' + (starIndex <= (hover || rating) ? 'text-amber-400' : '')}
+                    onClick={()=> setRating(starIndex)}
+                    onMouseEnter={() => setHover(starIndex)}
+                    onMouseLeave={() => setHover(rating)}>
+                      <span>&#9733;</span>
+                  </button>
+            )            
+          })}
+        </form>
+        <button className='text-indigo-600 hover:text-indigo-400 cursor-pointer transition duration-200' onClick={() => {
+                handleAddTodo(comment, rating)
+                setComment('')
+            }}> Submit</button>
+      </div>
+      
+      <ul className='flex flex-col flex-1 gap-1 p-4'>
+        { todos && todos.length > 0 ? (
+          todos?.map((todo, todoIndex) => (
+            <li key={todoIndex} className="max-w-[1000px] w-full mx-auto flex justify-between items-center bg-white mt-2 p-2 hover:shadow-lg rounded-3xl border-2 border-solid border-indigo-300">
+              <div className='flex ml-1 w-15'>
+                {headshot(todo)}
+              </div>
+              <div className={'flex flex-col ml-1 w-full ' + (todo.status == false ? ' opacity-50' : '')}>     
+                <span className='text-lg sm:text-xl font-semibold '>{todo.firstName} {todo.lastName}</span>
+                <span className='text-base sm:text-lg'>{todo.comment}</span>
+              </div>
+              <div className='flex p-2 ml-1 text-nowrap '>⭐ {todo.rating}/5</div>
+              <div className='flex flex-col items-center p-2 gap-2 '>
+                <button onClick={() => handleToggleStatus(todoIndex)}><i className={"text-indigo-600 hover:text-indigo-400 cursor-pointer transition duration-200 " + (todo.status == false ? " fa-solid fa-eye-slash opacity-50" : " fa-solid fa-eye")}></i></button>
+              </div>
+            </li>
+          ))) 
+          : (<p>Looks like something is missing</p>)
+        }
+      </ul>
+
+      <h2 className='text-indigo-800 text-center'>Display Below:</h2>
+
+      <ul className='flex flex-col flex-1 gap-1 p-4'>
+        {todos?.filter(todo => todo.status == true).map((todo, todoIndex) => (
+            <li key={todoIndex} className="max-w-[1000px] w-full mx-auto flex justify-between items-center bg-white mt-2 p-2 hover:shadow-lg rounded-3xl border-2 border-solid border-indigo-300">
+              <div className='flex ml-1 w-15'>
+                {headshot(todo)}
+              </div>
+              <div className='flex flex-col ml-1 w-full'>     
+                <span className='text-lg sm:text-xl font-semibold '>{todo.firstName} {todo.lastName}</span>
+                <span className='text-base sm:text-lg'>{todo.comment}</span>
+              </div>
+              <div className='flex p-2 ml-1 text-nowrap '>⭐ {todo.rating}/5</div>
+            </li>
+          ))}
+      </ul>
+    </div>
+  )
+
+}
+
+export default App
